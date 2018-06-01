9101f28f88a9b91d6f6504a6370bba69df10967f
Despair-lj <a691287671@gmail.com>
2018-06-01 16:28:06 +0800

    add postcss-preset-env

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,12 +2,15 @@ module.exports = {
     plugins: {
         'postcss-import': {},
         'postcss-url': {},
+        'postcss-preset-env': {
+            stage: 2,
+            browsers: '>1%'
+        },
         cssnano: {
             preset: 'advanced',
-            autoprefixer: false,
+            autoprefixer: true,
             'postcss-zindex': false
         }
-
         // to edit target browsers: use "browserslist" field in package.json
     }
 }
